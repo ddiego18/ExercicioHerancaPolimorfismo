c7ba3069fcd00d515a3c492764ce48e0da8a9494
ddiego18 <78805407+ddiego18@users.noreply.github.com>
2021-05-06 21:06:51 -0300

    Subir Código

---
+++ ExercicioHerancaPolimorfismo/Entities/ImportedProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+
+namespace ExercicioHerancaPolimorfismo.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double Customsfree { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsfree)
+            : base(name, price)
+        {
+            Customsfree = customsfree;
+        }
+
+      public double TotalPrice()
+        {
+            return Customsfree + Price;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs free: $ "
+                + Customsfree.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
